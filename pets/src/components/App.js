4b2dbe5374a143b3a47f02f0f3e055dbc534744f
César Eduardo Ortíz Salazar <pkgears@gmail.com>
2018-12-07 16:13:33 -0600

    Formulario y validación

--- pets/src/components/App.js
+++ pets/src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Header from './header'
+import AgregarCita from './agregar_cita'
 
 class App extends Component {
+
+  state = {
+    citas:[]
+  };
+
+  crearCita = (nuevaCita) => {
+    const citas = [...this.state.citas, nuevaCita];
+    this.setState({citas})
+    console.log(citas);
+  }
+
   render() {
     return (
-      <div className="App">
-       
+      <div className="container">
+        <Header titulo={"Administrador de pacientes de veterinaria"}/>
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita
+                crearCita= {this.crearCita}
+              />
+          </div>
+        </div>
       </div>
     );
   }
