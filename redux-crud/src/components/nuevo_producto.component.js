915ab5c186c0a454ae66ff87450030a631dd887f
César Eduardo Ortíz Salazar <pkgears@gmail.com>
2018-12-10 10:04:19 -0600

    Formulario para agregar item a API

--- redux-crud/src/components/nuevo_producto.component.js
+++ redux-crud/src/components/nuevo_producto.component.js
@@ -2,24 +2,63 @@ import React, {Component} from 'react';
 
 class NuevoProducto extends Component {
     
+    state = {
+        nombre: '',
+        precio: 0,
+        error: false
+    }
+
+    nombreProducto = e => {
+        this.setState({nombre: e.target.value});
+    }
+
+    precioProducto = e => {
+        this.setState({precio: e.target.value});
+    }
+
+    nuevoProducto = (e) => {
+        e.preventDefault();
+
+        const { nombre, precio } = this.state
+
+        if(!nombre && !precio ){
+            this.setState({error: true});
+            return;
+        } else {
+            this.setState({error:false})
+        }
+
+
+    }
+
     render(){ 
+        const error = this.state.error;
+
         return(
             <div className="row justify-content-center mt-5">
                 <div className="col-md-8">
                     <div className="card">
                         <div className="card-body">
                             <h2 className="text-center">Agregar Nuevo Producto</h2>
-                            <form>
+                            <form onSubmit={this.nuevoProducto}>
                                 <div className="form-group">
                                     <label>Titulo</label>
-                                    <input type="text" className="form-control" placeholder="Titulo" />
+                                    <input type="text" className="form-control" placeholder="Titulo"
+                                    onChange={this.nombreProducto}/>
                                 </div>
                                 <div className="form-group">
                                     <label>Precio del Producto</label>
-                                    <input type="text" className="form-control" placeholder="Precio" />
+                                    <input type="number" className="form-control" placeholder="Precio"
+                                    onChange={this.precioProducto}/>
                                 </div>
                                 <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Agregar</button>
                             </form>
+
+                            { error ? 
+                                <div className="font-weight-bold alert alert-danger text-center mt-4">Todos los campos son obligatorios</div> : ''
+                            
+                            }
+
                         </div>
                     </div>
                 </div>
